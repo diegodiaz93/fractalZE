6b7fec32ec8aa3d3d27bd07f281d7a62614cdfcc
Lopez Ezequiel <lopezezequiel.09@gmail.com>
2015-06-08 00:19:57 -0300

    no comments

--- FractalZE/__init__.py
+++ FractalZE/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 *-*
 """
 @author: López Ricardo Ezequiel
-@organization: UTN FRRE - Matemática Superior
-@copyright: no tiene
-@license: gratis
-@contact: lopezezequiel.09@gmail.com
-@bug: probablemente muchos, fue hecho en 2 dias
-@todo: agregar validacion, optimizar, eliminar redundacias, agregar fractales, agregar funciones
+@license: GNU GENERAL PUBLIC LICENSE
+@contact: mail@lopezezequiel.com
 """
